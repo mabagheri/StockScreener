80231042ee4f7c21207c86e01e98b1c5089530a4
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2025-11-11 14:39:18 -0500

    Update tickers list in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ else:
 tickers = ["XIC.TO", 
          "XLU"]
 tickers = ["SPY", "QQQ", "XIC.TO", 
-                "XLC", "XLY", "XLP", "XLE", "XLF", "XLV", "XLI", "XLK", "XLB", "XLRE", "XLU"]
+                "XLC", "XLY", "XLP", "XLE", "XLF", "XLV", "XLI", "XLK", "XLRE", "XLU"]
 
 today = datetime.today().date()
 start_date = datetime(2025, 11, 2).date()  # day after your last data
