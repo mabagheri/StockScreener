f2ed09535f40b0cccfc66ca4803d32fc2893cc7f
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2025-11-11 14:35:30 -0500

    Add progress bar and status text for data fetching

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,6 @@ else:
 # tickers = tickers_info["Ticker"].dropna().unique().tolist()
 tickers =  ["XIC.TO", 
              "XLU"]
-st.write(tickers)
 
 today = datetime.today().date()
 start_date = datetime(2025, 11, 2).date()  # day after your last data
@@ -39,7 +38,11 @@ results = []
 
 st.write(f"Fetching latest data since {start_date} ...")
 
-for ticker in tickers:
+progress_bar = st.progress(0)
+status_text = st.empty()
+
+for i, ticker in enumerate(tickers, start=1):      
+    
     csv_path = os.path.join(csv_folder, f"{ticker}.csv")
     if not os.path.exists(csv_path):
         st.warning(f"No CSV found for {ticker}, skipping.")
@@ -51,26 +54,18 @@ for ticker in tickers:
     df_old = df_old[df_old["Date"] <= pd.Timestamp("2025-11-01")]
 
     # Fetch recent data
-    # status_text.text(f"Downloading {ticker} ({i}/{len(tickers)}) ...")
+    status_text.text(f"Downloading {ticker} ({i}/{len(tickers)}) ...")
     df_new = yf.download(ticker, start=start_date, end=today + timedelta(days=1))
 
     df_new = df_new.iloc[:, :].reset_index(drop=False)
     df_new.columns = ["Date", "Close", "High", "Low", "Open", "Volume"]
     
-    # df_new.reset_index(inplace=True)
-    st.write("df_new", df_new.head(4))
-    st.write("df_old", df_old.head(4))
-
-
     # Combine
-    # df_new = df_new.rename(columns={"Date": "Date"})
     df_all = pd.concat([df_old, df_new], ignore_index=True).drop_duplicates(subset=["Date"])
     st.write(ticker, df_all.head(10))
 
     # Keep only the last n days
     cutoff = datetime.today() - timedelta(days=n_days)
-    st.write(df_all.head(5))
-    st.write(df_all.tail(4))
     
     df_window = df_all[df_all["Date"] >= cutoff]
 
@@ -87,7 +82,7 @@ for ticker in tickers:
         f"Highest ({n_option})": round(highest_price, 2),
         f"Drop % ({n_option})": round(drop_pct, 2)
     })
-    st.write(results)
+    # st.write(results)
 
 if results:
     df_results = pd.DataFrame(results)
