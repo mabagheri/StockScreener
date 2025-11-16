6b8a8fa66f3ebf6e01d2c0cab4de2c66f42aba24
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2025-11-15 23:45:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,7 +121,7 @@ if st.button("Run Stock Drop Analysis"):
     
             st.write("119, df_new")
             st.dataframe(df_new.tail(26))
-            df_new = df_new.iloc[2:, :].reset_index(drop=True)
+            df_new = df_new.iloc[2:, :].reset_index(drop=False)
             st.dataframe(df_new.tail(3))
             st.write("119", df_new.shape)
 
